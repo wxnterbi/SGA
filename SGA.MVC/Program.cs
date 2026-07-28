20eb00d34420d6929659fcf51763e23e8a9f4a57
wxnterbi <wandamedina286@gmail.com>
2026-07-28 18:09:31 -0400

    Implementación del MVC

---
+++ SGA.MVC/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using SGA.IOC;
+using SGA.Persistence.Context;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using SGA.Application.Validations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews()
+    .AddFluentValidation(fv =>
+    {
+        fv.RegisterValidatorsFromAssemblyContaining<UsuarioValidator>();
+    });
+
+builder.Services.AddDbContext<SGABD>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDependencyInjection();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
